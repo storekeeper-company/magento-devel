6e035ed1050311b12a5a3b132509906e395a82cf
Telepatrick <eugenekobiliatsky@gmail.com>
2024-04-04 11:57:37 +0300

    partial docker install bash script update

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "hello from "$HOSTNAME
+source env/blackfire.env
+source env/db.env
+source env/elasticsearch.env
+source env/magento.env
+source env/opensearch.env
+source env/phpfpm.env
+source env/rabbitmq.env
+source env/redis.env
 
-# todo move stuff from readme
+composer config --global http-basic.repo.magento.com "$MAGENTO_PUBLIC" "$MAGENTO_PRIVATE"
+composer create-project --repository-url=https://repo.magento.com/ \
+     "${META_PACKAGE}" /tmp/exampleproject "${META_VERSION}"
+
+cp /tmp/exampleproject/* /var/www/html/
+ rm -rf /tmp/exampleproject/
